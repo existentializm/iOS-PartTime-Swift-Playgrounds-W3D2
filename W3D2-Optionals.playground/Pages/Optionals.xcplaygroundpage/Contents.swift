bc950d79549a23f582debea2d6b043d2f49cf856
existentializm <e.jaluague@gmail.com>
2017-05-24 19:51:02 -0700

    Progress

--- W3D2-Optionals.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ W3D2-Optionals.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -49,6 +49,7 @@ var convertedNum = Double(numberString) //convert numberString to a Double
 var inputString = "hello"
 // try to convert inputString to an Int here:
 
+var convertedString = Int(inputString)
 
 //: If you've done everything right you should see "nil" along the right-hand gutter of the playground. This is what we use to represent "nothing". Only optional variables can have a value of `nil`.
 //:
@@ -61,7 +62,8 @@ var inputString = "hello"
 //: Then, change the string "1" to "🔥" and observe how the playground responds (or doesn't respond)
 
 var number = Int("1")
-// number + 1 // uncomment and fix this.
+number! + 1 // uncomment and fix this.
+number! = "🔥"
 
 
 //: In general, force unwrapping is always a risk for a crash, and should be used sparingly and with caution.
